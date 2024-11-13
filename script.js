182d47fc13728f446af5bd2fc2a4770eb0e6771b
homer393495 <147638179+homer393495@users.noreply.github.com>
2024-11-13 12:17:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const newsContainer = document.getElementById('news-container');
 
+async function fetchNews() {
+    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY');
+    const data = await response.json();
+    displayNews(data.articles);
+}
+
+function displayNews(articles) {
+    newsContainer.innerHTML = '';
+    articles.forEach(article => {
+        const articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+        articleDiv.innerHTML = `
+            <h2>${article.title}</h2>
+            <p>${article.description || 'No description available.'}</p>
+            <a href="${article.url}" target="_blank">Read more</a>
+        `;
+        newsContainer.appendChild(articleDiv);
+    });
+}
+
+fetchNews();
